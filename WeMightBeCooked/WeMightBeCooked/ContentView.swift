f637f0099fe84e41876fae0a8ec0f176ca2ac309
Michelle Dong <99102209+MICH3LL3D@users.noreply.github.com>
2026-06-21 02:01:37 -0700

    Add files via upload

---
+++ WeMightBeCooked/WeMightBeCooked/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  WeMightBeCooked
+//
+//  Created by Michelle Dong on 6/20/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        StreamingDemoView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
